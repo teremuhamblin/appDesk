ad2961f843f393eeed0e713dd5959b32457baf2e
98731 <105843577+teremuhamblin@users.noreply.github.com>
2026-08-26 23:44:48 +0200

    Add App component with PluginStore integration

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import PluginStore from "./plugin-store";
+
+function App() {
+  return (
+    <div>
+      {/* ton UI existante */}
+      <PluginStore />
+    </div>
+  );
+}
+
+export default App;
